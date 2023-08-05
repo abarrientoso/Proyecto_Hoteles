1589dd6a8b631a309eed9351ea3218101a7c21db
naya2904 <105831356+naya2904@users.noreply.github.com>
2023-08-04 19:54:06 -0600

    Se agregan vistas y funcionalidad de hoteles

---
+++ ProgramacionAvanzada_Proyecto_G2/Controllers/HotelController.cs
@@ -0,0 +1,47 @@
+﻿using ProgramacionAvanzada_Proyecto_G2.Entities;
+using ProgramacionAvanzada_Proyecto_G2.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ProgramacionAvanzada_Proyecto_G2.Controllers
+{
+    public class HotelController : Controller
+    {
+        public ActionResult Hotel1()
+        {
+            return View("Hotel1");
+        }
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Hotel2()
+        {
+            return View("Hotel2");
+        }
+
+        public ActionResult Hotel3()
+        {
+            return View("Hotel3");
+        }
+
+        public ActionResult Hotel4()
+        {
+            return View("Hotel4");
+        }
+
+        public ActionResult Hotel5()
+        {
+            return View("Hotel5");
+        }
+
+        public ActionResult Hotel6()
+        {
+            return View("Hotel6");
+        }
+
+    }
+}
