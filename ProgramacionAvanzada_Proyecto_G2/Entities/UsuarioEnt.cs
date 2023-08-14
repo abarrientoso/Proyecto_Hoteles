b2fbad167685f121ed7d5d9db9396f29507e1b2b
andre <andres@abarrientoso.com>
2023-08-14 15:08:03 -0600

    Modificado usuario entidad, modificados modelos de las vistas y modificado HomeController para las consultas y asignacion de roles a usuarios

--- ProgramacionAvanzada_Proyecto_G2/Entities/UsuarioEnt.cs
+++ ProgramacionAvanzada_Proyecto_G2/Entities/UsuarioEnt.cs
@@ -9,13 +9,12 @@ namespace ProgramacionAvanzada_Proyecto_G2.Entities
     {
         public long idUsuario { get; set; }
         public string correoElectronico { get; set; }
-        public string contrasenna { get; set; }
+        public string contrasena { get; set; }
         public string identificacion { get; set; }
         public string nombre { get; set; }
-        public string contrasennaNueva { get; set; }
-        public string confirmarContrasenna { get; set; }
+        public string apellido { get; set; }
         public bool estado { get; set; }
-        public byte idRol { get; set; }
+        public int idRol { get; set; }
         public string nombreRol { get; set; }
     }
 }
